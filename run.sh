63ab4392af885c155234ba095e91003e46e60efb
Xinkun Nie <xnie@mit.edu>
2017-12-16 22:42:34 -0800

    cleaned up run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-D=3
-total_repeats=6
+D=10
+total_repeats=1000
 repeats=$((total_repeats / D))
 for i in $(seq 1 $D)
 do
-    python dev/main.py -s all -a lil-ucb -r $repeats -t 40 -g 20 -z 0.01 -u 20 -m 30 -c 1.0 -e single-debias -x 0.5 -b 1.0 -w 0.1 -k $i -p -y -o out -v
+    python dev/main.py -s all -a lil-ucb -r $repeats -t 40 -g 100 -z 0.01 -u 20 -m 30 -c 1.0 -e single-debias -x 0.5 -b 1.0 -w 0.1 -k $i -p -y -o out -v
 done
 
 python dev/process_div.py -i out -a lil-ucb -x 0.5 -b 1.0 -s all -t 40 -w 0.1 -e single-debias -d gauss -k $D -r $total_repeats 
-#for i in $(seq 1 10)
-#do
-#    python dev/main.py -s all -a lil-ucb -r 50 -t 40 -g 100 -z 0.01 -u 20 -m 30 -c 1.0 -e single-debias -x 0.5 -b 1.0 -w 0.1 -k $i -p -y -o out -v
-#done
-#
-#python dev/process_div.py -i out -a lil-ucb -x 0.5 -b 1.0 -s all -t 40 -w 0.1 -e single-debias -d gauss -k 10 -r 400 
